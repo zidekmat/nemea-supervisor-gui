f03d397b4f334a2ff2c974e7c31fffd31b84d9eb
Matej Zidek <zidekmat@fit.cvut.cz>
2018-01-15 11:17:24 +0100

    cleanup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,30 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
 import router from './router'
 
-Vue.config.productionTip = false
+import ConfComponent from './components/Conf.vue'
+import ConfForm from './components/ConfForm.vue'
+import ConfListing from './components/Listing.vue'
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
+require('../node_modules/font-awesome/css/font-awesome.min.css')
+
+Vue.use(Vuetify)
+Vue.component('conf-component', ConfComponent)
+Vue.component('conf-form-component', ConfForm)
+Vue.component('conf-listing-component', ConfListing)
+
+const app = new Vue({
   router,
-  template: '<App/>',
-  components: { App }
-})
+  render: h => h(App),
+  mounted () {
+  },
+  methods: {
+    goBack () {
+      window.history.length > 1
+        ? this.$router.go(-1)
+        : this.$router.push('/')
+    }
+  }
+}).$mount('#app')
